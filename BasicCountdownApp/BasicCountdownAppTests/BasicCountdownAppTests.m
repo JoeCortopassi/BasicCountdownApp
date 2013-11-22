ab4f73105001c95490538cb041b4c5f1d6452b18
Joe Cortopassi <joe@joecortopassi.com>
2013-11-21 21:44:03 -0800

    Initial commit

---
+++ BasicCountdownApp/BasicCountdownAppTests/BasicCountdownAppTests.m
@@ -0,0 +1,34 @@
+//
+//  BasicCountdownAppTests.m
+//  BasicCountdownAppTests
+//
+//  Created by Joe Cortopassi on 11/21/13.
+//  Copyright (c) 2013 Joe Cortopassi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BasicCountdownAppTests : XCTestCase
+
+@end
+
+@implementation BasicCountdownAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
