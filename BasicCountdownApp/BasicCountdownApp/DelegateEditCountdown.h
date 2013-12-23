48bd9356d9e43c6ec3924e08ab56c263654e3e90
Joseph Cortopassi <cortopasta@joecortbpretina.home>
2013-12-23 15:53:17 -0800

    Edit Title and Edit Date now set the edit view controllers countdown

---
+++ BasicCountdownApp/BasicCountdownApp/DelegateEditCountdown.h
@@ -0,0 +1,13 @@
+//
+//  DelegateEditCountdown.h
+//  BasicCountdownApp
+//
+//  Created by Joseph Cortopassi on 12/23/13.
+//  Copyright (c) 2013 Joe Cortopassi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DelegateEditCountdown <NSObject>
+
+@end
